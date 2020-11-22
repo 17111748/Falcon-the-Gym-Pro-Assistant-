560c26333ee90148799b6a2a005da39c614e9d6f
Venkata Vivek Thallam <venkatavivekthallam@Venkata-Viveks-MacBook-Pro.local>
2020-11-22 15:05:44 -0500

    Updated spotify
    Updated spotify so that it finds device id based on which one is
    active and tries to pause or play

--- UI/spotify.py
+++ UI/spotify.py
@@ -1,7 +1,7 @@
 import spotipy
 import spotipy.util as util
 
-scope = 'user-read-playback-state, user-modify-playback-state'
+scope = 'user-read-playback-state user-modify-playback-state'
 
 token = util.prompt_for_user_token("Venkata Vivek Thallam", scope,
 client_id='31783ef36dc04e8589a58e30bbe327fe',
@@ -10,7 +10,18 @@ redirect_uri='http://localhost:8888/callback')
 
 sp = spotipy.Spotify(auth=token)
 
-device_id = sp.devices()['devices'][0]['id']
+devices = sp.devices()['devices']
+device_id = None
+for i in range(len(devices)):
+    if(devices[i]['is_active']):
+        device_id = devices[i]['id']
 
-sp.start_playback(device_id)
+if device_id != None:
+    try:
+        sp.start_playback(device_id)
+    except:
+        sp.pause_playback(device_id)
+else:
+    print("Nothing playing!")
 
+print(sp.current_playback())
